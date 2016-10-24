615f12b2de700b9006f809033b70ab5d7a9742d9
tonioecto <antoninleroux@sfr.fr>
2016-10-24 18:00:19 +0200

    add a previous speaker database and enable the speaker page to display the speakers by the year they came

--- event/models.py
+++ event/models.py
@@ -7,3 +7,10 @@ class Speaker(models.Model):
     image = models.FileField("Image :", upload_to='media/speakers')
     def __str__(self):              # __unicode__ on Python 2
         return self.name
+class PreviousSpeaker(models.Model):
+    name = models.CharField("Nom du speaker", max_length=254)
+    description = models.TextField("Description", blank=True)
+    image = models.FileField("Image :", upload_to='media/speakers')
+    year=models.CharField("Annee ou le speaker est passe a tedx", max_length=30)
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
