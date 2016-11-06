de6230746c79cb00ad1ad7f45f29770b8601b320
tonioecto <antoninleroux@sfr.fr>
2016-11-06 16:00:28 +0100

    add a new field to the speakers

--- event/models.py
+++ event/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Speaker(models.Model):
     name = models.CharField("Nom du speaker", max_length=254)
     description = models.TextField("Description", blank=True)
+    resume=models.CharField("Une description rapide", max_length=254, blank=True)
     image = models.FileField("Image :", upload_to='assets/media/media/speakers')
     def __str__(self):              # __unicode__ on Python 2
         return self.name
@@ -11,6 +12,7 @@ class Speaker(models.Model):
 class PreviousSpeaker(models.Model):
     name = models.CharField("Nom du speaker", max_length=254)
     description = models.TextField("Description", blank=True)
+    resume=models.CharField("Une description rapide", max_length=254, blank=True)
     image = models.FileField("Image :", upload_to='assets/media/media/speakers')
     year=models.CharField("Annee ou le speaker est passe a tedx", max_length=30)
     def __str__(self):              # __unicode__ on Python 2
